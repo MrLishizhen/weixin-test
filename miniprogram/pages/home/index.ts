4c9c20ab451b73e0346035c120a4049ed3b805d2
Mrlishizhen <718647063@qq.com>
2023-03-15 18:15:35 +0800

    first commit

---
+++ miniprogram/pages/home/index.ts
@@ -0,0 +1,70 @@
+// pages/home/index.ts
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    swiper_list:[{name:'孙悟空',color:'red'},{name:'猪八戒',color:'#ccc'},{name:'天蓬元帅',color:'#b0b0b0'}],
+    swiper_data:{
+      indicatorDots:true,
+
+    }
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {
+
+  }
+})
